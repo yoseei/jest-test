ccec785badfe2a2a00825c4fe4b5d3facf86c801
yoseei <katashiyoika@gmail.com>
2022-05-31 18:45:41 +0900

    hoge

--- src/components/Hello.spec.tsx
+++ src/components/Hello.spec.tsx
@@ -5,6 +5,7 @@ import Hello from "./Hello";
 it('renders "Hello World"', () => {
   render(<Hello />);
   const myElement = screen.getByText(/Hello World/);
+
   screen.debug();
   expect(myElement).toBeInTheDocument();
 });
